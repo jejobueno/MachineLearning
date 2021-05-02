93edd6fb7e7a95849e118138e59671984d383667
Jesus Bueno <jessbuenos@gmail.com>
2021-05-02 13:58:30 +0200

    sigmoid function and plotData

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -13,7 +13,14 @@ figure; hold on;
 %
 
 
-
+% Find Indices of Positive and Negative Examples in the vector y
+pos = find(y==1); neg = find(y == 0);
+% Plot Examples of the indexes obtained from y
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y','MarkerSize', 7);
+xlabel('Exam 1 score');
+ylabel('Exam 2 score');
+legend('Admited','Not admited')
 
 
 
